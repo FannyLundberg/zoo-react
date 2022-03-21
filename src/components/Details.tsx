f04397f36d531cf16499f40f997ce85f7bdaa653
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-03-21 15:10:38 +0100

    WIP

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,76 +1,174 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Animal } from "../models/Animal";
-import { IAnimal } from "../models/IAnimal";
 import "./Details.css";
 
 export function Details() {
     
     const [animal, setAnimal] = useState<Animal[]>([]);
+    const [animals, setAnimals] = useState<Animal[]>([]);
     const [fedTime, setfedTime] = useState(Date);
     const [fed, setFed] = useState(false);
+    const [feedAnimal, setFeedAnimal] = useState(true);
+    let animalsList: [] = [];
 
-
+    // Funktion ska köras direkt när man kommer till sidan
     useEffect(() => {
         getAnimalFromLs();
     }, []);
 
-
-    // Hämta från ls
+    // Hämta från localStorage
     function getAnimalFromLs() {
         let animalObject = localStorage.getItem("animalsLs") || "[]";
         let lsAnimal = JSON.parse(animalObject);
 
-        console.log(lsAnimal)
+        let animalsObject = localStorage.getItem("animalsList") || "[]";
+        let lsAnimals = JSON.parse(animalsObject);
 
         setAnimal(lsAnimal)
+        setAnimals(lsAnimals)
     }
 
+    // Visa ruta om längre än fyra timmar sedan matad
+    let infoHungry =
+    <></>
+    if (feedAnimal) {
+        infoHungry = <p> Jag är hungrig, mata mig! </p>
+    }
 
-    // Om matad ska man inte kunna mata igen
-    let ableToFed =
-    <button className="fedBtn" onClick={fedAnimal}>Mata </button>
+    // Visa status om matad eller ej
+    let foodStatus =
+    <span> Hungrig </span>
     if (fed) {
-        ableToFed = <button className="fedBtn" disabled>Redan matad</button>
-    } 
-
+        foodStatus = <span> Mätt </span>
+    }
 
     // Vilken info som ska skrivas ut
     let dataLs = animal.map((animal: Animal) => {
         return (
-            <div className="animalDiv" key={animal.name} >
+            <div className="animalDiv" key={animal.id} >
                 <h2> Namn: {animal.name} </h2>
                 <p> Födelseår: {animal.yearOfBirth} </p>
                 <p> Latin: {animal.latinName} </p>
                 <p> Beskrivning: {animal.longInfo} </p>
                 <p> Mediciner: {animal.medicine} </p>
                 <img className="animalImg" src={animal.imgUrl} ></img>
-                <p>{animal.isFed}</p>
-                {ableToFed}
+                <p>Status: {foodStatus} </p>
+                {!fed && <button className="fedBtn" onClick={() => fedAnimal(animal.id)}>Mata djur</button>}
+                {/* {ableToFed} */}
                 <p>Senast matad: {fedTime} </p>
             </div>
         );
     });
 
-
     // Vid klick på Mata-knappen
-    function fedAnimal() {
+    function fedAnimal(id: number) {
 
-        let fedTime = new Date();
-        console.log(fedTime)
+        console.log(id)
 
+        // Sätter tiden för matningen
+        setfedTime(Date);
+        let feedTime = new Date;
+
+        // Sätter till matad till true
         setFed(!fed);
+        let fedBool = !fed;
 
-        console.log(fed)
+        // Sätter till matad till true
+        setFeedAnimal(!feedAnimal);
 
-        // setfedTime(fedTime);
+        // Köra funktion för att spara i ls
+        let animalObject = {
+            id: animal[0].id, 
+            name: animal[0].name, 
+            latinName: animal[0].latinName, 
+            yearOfBirth: animal[0].yearOfBirth, 
+            shortInfo: animal[0].shortInfo, 
+            longInfo: animal[0].longInfo,
+            imgUrl: animal[0].imgUrl,
+            medicine: animal[0].medicine,
+            isFed: fedBool,
+            lastFed: feedTime
+        };
+        
+        saveToLs(animalObject);
+
+        // Timer som gör det möjligt att mata igen efter angiven tid
+        setTimeout(() => {
+
+            setFed(fed);
+
+            let fedBool = fed;
+
+            let animalObject = {
+                id: animal[0].id, 
+                name: animal[0].name, 
+                latinName: animal[0].latinName, 
+                yearOfBirth: animal[0].yearOfBirth, 
+                shortInfo: animal[0].shortInfo, 
+                longInfo: animal[0].longInfo,
+                imgUrl: animal[0].imgUrl,
+                medicine: animal[0].medicine,
+                isFed: fedBool,
+                lastFed: feedTime
+            };
+            // setFed(fed);
+
+            saveToLs(animalObject);
+        // }, 10800000)
+        }, 10000)
+
+        // Timer som visar om det gått mer än fyra timmar sedan matning
+        setTimeout(() => {
+            setFeedAnimal(feedAnimal);
+        // }, 14400000)
+        }, 12000)
     }
 
+    // Funktion för att spara till localStorage
+    function saveToLs(fedAnimals: any) {
+        console.log(fedAnimals);
 
+        let animalsObject = localStorage.getItem("animalsList") || "[]";
+        let animalsList = JSON.parse(animalsObject);
+
+        // for (let i = 0; i < animalsList.length; i++) {
+        //     if(!fedAnimals.some((a: any) => a.id === animalsList[i].id)) {
+        //         animalsList.push(fedAnimals)
+        //         localStorage.setItem("animalsList", JSON.stringify(animalsList));
+        //     } else {
+        //         for (let j = 0; j < fedAnimals.length; j++) {
+        //             if (fedAnimals[j].id === animalsList[i].id) {
+        //                 animalsList[i].isFed = false;
+        //                 localStorage.setItem("animalsList", JSON.stringify(animalsList));
+        //             }
+        //         }
+        //     }
+        // }
+
+        // if (!fedAnimals.some((fedAnimals) => fedAnimals.id === animalsList)) {
+        //     animalsList.isFed = false;
+        // } else {
+        //     animalsList.push(fedAnimals);
+        //     localStorage.setItem("animalsList", JSON.stringify(animalsList));
+        // }
+
+        // animalsList.push(fedAnimals);
+        // localStorage.setItem("animalsList", JSON.stringify(animalsList));
+
+        console.log(animalsList);
+    }
+
+    // Rendera HTML
     return (
         <section>
+            <div id="feedAnimalInfo">
+                {infoHungry}
+            </div>
             {dataLs}
-            <p><Link to="/">Tillbaka till startsidan</Link></p>
+            <p>
+                <Link to="/">Tillbaka till startsidan</Link>
+            </p>
         </section>
     );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,13 +9,13 @@ export function Home() {
 
     const [animal, setAnimal] = useState<Animal[]>([]);
     let animalList: IAnimal[] = [];
-    // let animals: IAnimal[] = [];
-    // let clickedAnimal: Animal;
 
+    // Funktion ska köras direkt när man kommer till sidan
     useEffect(() => {
         getData();
     }, []);
 
+    // Hämta data från API:t
      function getData() {
         axios
         .get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
@@ -36,24 +36,10 @@ export function Home() {
                 )
             });
             setAnimal(dataFromApi);
-
-            // let data = response.data;
-
-            // for (let i = 0; i < data.length; i++) {
-                
-            //     animalList.push(data[i]);
-            //     localStorage.setItem("animalLs", JSON.stringify(animalList));
-            // }   
-            // console.log(animalList)
-            // getFromLs();
         }
     )};
 
-    // function getFromLs() {
-    //     let animalsFromLs = localStorage.getItem("animalsLs") || "[]";
-    //     animals = JSON.parse(animalsFromLs);
-    // }
-
+    // Vad som ska presenteras om respektive djur i listan
     let dataApi = animal.map((animal: Animal) => {
         return (
             <div className="animalsDiv" key={animal.id}>
@@ -66,6 +52,7 @@ export function Home() {
         );
     });
 
+    // Vid klick på Mer om djuret
     function moreInfoBtn(id: any) {
         console.log("Klick på knapp");
 
@@ -73,6 +60,7 @@ export function Home() {
         saveToLs(clickedAnimal);
     };
 
+    // Spara till localStorage
     function saveToLs(clickedAnimal: any) {
         console.log(clickedAnimal);
 
@@ -80,102 +68,10 @@ export function Home() {
         localStorage.setItem("animalsLs", JSON.stringify(animalList));
     };
 
+    // Rendera HTML
     return (
         <section>
               {dataApi}
         </section>
     );
-
-
-
-
-    // const [animals, setAnimals] = useState<Animal[]>([]);
-    // let animal: IAnimal[] = [];
-    // let animalList: IAnimal[] = [];
-
-    // useEffect(() => {
-    //     getData();
-    // }, []);
-    // // }, [animals]);
-
-    // function getData() {
-    //     axios
-    //     .get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
-    //     .then((response) => {
-    //         renderData(response.data)
-
-    //         let data = response.data;
-
-    //         for (let i = 0; i < animalList.length; i++) {
-
-    //             let animals = localStorage.getItem("animalLs") || "[]";
-    //             animalList = JSON.parse(animals);
-    
-    //             animalList.push(data[i]);
-    //             localStorage.setItem("animalLs", JSON.stringify(animalList));
-    //         }   
-
-    //         // let dataFromApi = response.data.map((animal: IAnimal, i) => {
-    //         //     return ( 
-    //         //         <div key={i}>
-    //         //             <h2>Namn: {animal.name}</h2>
-    //         //             <p> Födelseår: {animal.yearOfBirth}</p>
-    //         //             <p>{animal.shortDescription}</p>
-    //         //             <img src={animal.imageUrl}></img>
-    //         //             <button onClick={moreInfoBtn}><Link to="/details">Mer om {animal.name}</Link></button>
-    //         //         </div>
-    //         //     );  
-    //         // })
-    //         // setAnimals(dataFromApi);
-    //     }
-    // )};
-
-    // function renderData(data: IAnimal[]) {
-        
-    //     let dataFromApi = data.map((animal: IAnimal, i) => {
-    //         return ( 
-    //             <div key={i} id="animalDiv">
-    //                 <h2>Namn: {animal.name}</h2>
-    //                 <p> Födelseår: {animal.yearOfBirth}</p>
-    //                 <p>{animal.shortDescription}</p>
-    //                 <img src={animal.imageUrl}></img>
-    //                 <button><Link to="/details">Mer om {animal.name}</Link></button>
-    //                 {/* <button onClick={moreInfoBtn}><Link to="/details">Mer om {animal.name}</Link></button> */}
-    //             </div>
-    //         );  
-            
-    //     })
-    //     // setAnimals(dataFromApi); 
-    //     // setFunction();
-    //     console.log(data)
-    //     console.log(dataFromApi)
-    // };
-
-    // // function moreInfoBtn(i: number) {
-    // //     console.log("Klick");
-
-    // //     // let oneAnimal = animal[i];
-
-    // //     // saveToLs(oneAnimal);
-    // // }
-
-
-    // // function saveToLs(oneAnimal: IAnimal) {
-    // //     let animalLs = localStorage.getItem("animalsLs") || "[]";
-    // //     animalLs = JSON.parse(animalLs);
-
-    // //     animalLs.push(oneAnimal);
-    // //     localStorage.setItem("productCart", JSON.stringify(animalLs));
-    // // }
-
-
-    // // function setFunction() {
-    // //     setAnimals(dataFromApi);
-    // // }
-
-    // return (
-    //     <section>
-    //         <article>{animals}</article>
-    //     </section>
-    // );
 }
